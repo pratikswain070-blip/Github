daead1ae5026fb9078bae0e643543cbdaf1cd26b
Pratik12 <pratikswain070@gmail.com>
2025-11-03 14:12:21 +0530

    first commit

---
+++ python1.py
@@ -0,0 +1,29 @@
+"""
+
+A= input("Enter your name:")
+print(A)
+
+
+
+B= float(input("Enter the length in cm "))
+print(B)
+C= float(input("Enter the breadth in cm "))
+print(C)
+area= B*C
+print("Area of retangle is:", area)
+
+print("Hello pratik ", end="")
+ 
+print(70)
+print("Hi")
+print("hello :" , A ,B )
+
+name = "harish"
+age = 50
+print(f"hello {name} your age is {age} ")
+"""
+
+a= int(input("number1 da:"))
+b= int(input("dusra no bata :"))
+c= a+b
+print(f" tera answer ya {a} aur {b} ko add karna ba ya hai {c} ")
