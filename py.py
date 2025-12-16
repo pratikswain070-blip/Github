0df6ba4003658d281b1917873aff138823ebca69
pratikswain070-blip <pratikswain070@gmail.com>
2025-12-16 15:24:18 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,35 @@
+"""
+print("hello bhai" , 43 , "november" , sep="/ ")
+
+a = print(input("enter something:",))
+print(type(a))
+"""
+
+a = 30
+print(type(a))
+
+b= 90.5
+print(type(b))
+
+c= 'hello'
+print(type(c))
+
+d= True 
+print(type(d))
+
+e= None 
+print(type(e))
+
+f = [3, 4 , 6, 2, 7]
+print(type(f))
+
+g = {3, 8 , 54 , 75}
+print(type(g))
+
+h= ("hello",456, 980.7)
+print(h[0])
+print(type(h))
+
+
+q= eval(input("enter something: "))
+print(type(q)
